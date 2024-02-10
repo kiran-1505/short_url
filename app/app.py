0e69711c7ca5cc43e9972cd6992975e8400ab022
Kiran B K <118459995+kiran-1505@users.noreply.github.com>
2024-02-10 16:46:13 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify
+
+
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import string
 import random
@@ -9,11 +11,17 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+
 # Create a database model for URLs
 class Urls(db.Model):
     id_ = db.Column("id_", db.Integer, primary_key=True)
     long = db.Column("long", db.String())
     short = db.Column("short", db.String(4))
+
+
+with app.app_context():
+    db.metadata.create_all(bind=db.engine, checkfirst=True)
+
 
 def create_shorten_url():
     letters = string.ascii_lowercase + string.ascii_uppercase
@@ -23,6 +31,7 @@ def create_shorten_url():
         short_url = Urls.query.filter_by(short=rand_letters).first()
         if not short_url:
             return rand_letters
+
 
 def get_or_create_short_url(long_url):
     found_url = Urls.query.filter_by(long=long_url).first()
@@ -35,22 +44,16 @@ def get_or_create_short_url(long_url):
         db.session.commit()
         return short_url
 
+
 @app.route("/shorten", methods=["POST"])
 def shorten_url():
     """
     Let's a developer create a short url from a long one
     and if it already exists then just return
-    
     Input and output will be in JSON format
     """
-    data = request.json
-    long_url = data.get('long_url')
-    if long_url:
-        short_url = get_or_create_short_url(long_url)
-        response = {"long_url": long_url, "short_url": short_url}
-        return jsonify(response), 200
-    else:
-        return jsonify({"error": "Missing long_url parameter"}), 400
+    pass
+
 
 @app.route("/long_url", methods=["GET"])
 def get_long_url():
@@ -58,15 +61,8 @@ def get_long_url():
     Let's a developer get the long url from a short one
     Input and output will be in JSON format
     """
-    short_url = request.args.get('short_url')
-    if short_url:
-        found_url = Urls.query.filter_by(short=short_url).first()
-        if found_url:
-            return jsonify({"short_url": short_url, "long_url": found_url.long}), 200
-        else:
-            return jsonify({"error": "Short URL not found"}), 404
-    else:
-        return jsonify({"error": "Missing short_url parameter"}), 400
+    pass
+
 
 @app.route("/delete_url", methods=["DELETE"])
 def delete_url():
@@ -74,18 +70,8 @@ def delete_url():
     Let's a developer delete a url from the database
     Input and output will be in JSON format
     """
-    data = request.json
-    short_url = data.get('short_url')
-    if short_url:
-        url_to_delete = Urls.query.filter_by(short=short_url).first()
-        if url_to_delete:
-            db.session.delete(url_to_delete)
-            db.session.commit()
-            return jsonify({"message": f"Short URL {short_url} deleted successfully"}), 200
-        else:
-            return jsonify({"error": "Short URL not found"}), 404
-    else:
-        return jsonify({"error": "Missing short_url parameter"}), 400
+    pass
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
